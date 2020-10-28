423be848b99a54e6589a40009a92056fcf08cf38
CircuitBreaker8 <braxtoneidem@gmail.com>
2020-10-28 16:41:48 -0700

    added segment and room w/ methods

--- group17Deadwood.java
+++ group17Deadwood.java
@@ -3,12 +3,53 @@ import java.io.*;
 
 
 class player extends actor {
+    void move(){
+
+    } //End Move
+
+    void work(){
+
+    } // End Work
+
+    void upgrade() {
+
+    } // End Upgrade
+
+    int diceRoll(){
+        return -1;
+    } // end Dice Roll
 
 } // end player
 
-class banker extends actor {
 
+abstract class banker extends actor {
+    abstract void startUp();
+    abstract void endOfDay();
+    abstract void endOfGame();
+    
+    
 } // end banker
+
+
+
+
+
+abstract class segment extends board {
+    abstract void fillSegment();
+    abstract void fillRoom();
+    abstract void findAvalibleMoves();
+    abstract void closeScene();
+
+} // end segment
+
+
+abstract class room extends segment{
+    abstract void shotCounterDone();
+} // End Room
+
+
+
+
 
 
 
@@ -17,6 +58,8 @@ class group17Deadwood{
 
 
     
+
+
 
 
     public static void main(String[] args) throws Exception{
