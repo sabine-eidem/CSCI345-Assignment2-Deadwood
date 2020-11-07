516798c56ac8e59e9ee2d44727880a9d268faefd
CircuitBreaker8 <braxtoneidem@gmail.com>
2020-11-06 22:48:49 -0800

    xlm into

--- Group17Deadwood.java
+++ Group17Deadwood.java
@@ -4,12 +4,19 @@ public class Group17Deadwood {
     
 
     public static void main(String args[]){
+        //Make array of class Player called players of size how many players
         int playerCount;
+        String newName;
         Scanner in = new Scanner(System.in);
         System.out.println("Welcome to Deadwood!\nHow many players? (2-6)");
         playerCount = in.nextInt();
-        System.out.print(playerCount);
+        Player[] Players = new Player[playerCount];
+        for(int i = 0; i < playerCount; i++){
+            System.out.print("Player " + i+1 + "what's your name: ");
+            newName = in.nextLine();
+            Players[1] = new Player(newName);
+        }
 
-
+        in.close();
     }
 }
